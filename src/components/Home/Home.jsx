a44e7c85d279daa8f3dd4aad77b86d4aa911559c
Anastasia_S <sivokonanastasia1@gmail.com>
2025-10-25 18:35:43 +0300

    Home, About

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,17 @@
+import s from './Home.module.css';
+
+const Home = () => {
+  return (
+    <div className={s.home}>
+      <div className={s.imgContainer}>
+        <img src="../../../public/геотермальний насос.jpg" alt="" />
+      </div>
+      <div className={s.text}>
+        <h1 className={s.h1}>Екологічне тепло для дому</h1>
+        <p className={s.p}>Теплові насоси - сучасна технологія, що дозволяє економити ресурси та зменшувати вплив на довкілля.</p>
+      </div>
+    </div>
+  )
+}
+
+export default Home;
