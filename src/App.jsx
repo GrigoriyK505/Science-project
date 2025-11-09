087b3685fd85c60bacc7ddc86acda2bf9f3aaca6
Anastasia_S <sivokonanastasia1@gmail.com>
2025-11-09 18:15:17 +0200

    survey

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import Home from './components/Home/Home.jsx';
 import About from './components/About/About.jsx';
 import Calculate from './components/Calculate/Calculate.jsx';
 import Footer from './components/Footer/Footer.jsx';
+import Survey from './components/Survey/Survey.jsx';
 const App = () => {
   return (
     <div>
       <Home />
       <About />
+      <Survey />
       <Calculate />
       <Footer />
     </div>
